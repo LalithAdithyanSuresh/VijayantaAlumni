b217006fc3ead402b9c92f352c436afc77edd4bf
Lalith Adithyan Suresh <140822743+LalithAdithyanSuresh@users.noreply.github.com>
2024-09-08 20:28:11 +0530

    implementing Google Sign in

--- src/components/PageChoose.jsx
+++ src/components/PageChoose.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import Hero from './Hero.jsx';
+import SignUp from './SignUp.jsx';
 
 function PageChoose({x}) {
     if(x==0){
         return <Hero />
+    }else if(x==3){
+        return <SignUp />
     }
     else{
         return(
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Navbar from './components/Navbar.jsx'
+import { GoogleOAuthProvider } from "@react-oauth/google"
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Navbar></Navbar>
-  </StrictMode>,
+  <GoogleOAuthProvider clientId='1009282809407-sh8h2kgmot2q295a503sl5530pldnaj9.apps.googleusercontent.com'>
+    <StrictMode>
+      <Navbar></Navbar>
+    </StrictMode>
+  </GoogleOAuthProvider>
 )
