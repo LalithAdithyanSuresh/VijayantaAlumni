b04d9b8a11af1191b48c1e08a804f96be8ede428
Lalith Adithyan Suresh <140822743+LalithAdithyanSuresh@users.noreply.github.com>
2024-09-08 13:35:49 +0530

    PartialHeroSection

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+function Hero() {
+    return (
+        <div className='w-screen h-screen flex justify-center items-center'>
+            <div className='flex flex-col justify-center'>
+                <p className='text-center text-[#3ABEF9]'>WELCOME TO</p>
+                <p>Vijayanta Schools Alumni Association</p>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Navbar from './Navbar.jsx'
+import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navbar></Navbar>
+    <Hero></Hero>
   </StrictMode>,
 )
