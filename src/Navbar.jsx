02120468bd2d7fcd11b25149bd6c560e2ad26b74
Lalith Adithyan Suresh <140822743+LalithAdithyanSuresh@users.noreply.github.com>
2024-09-08 11:07:19 +0530

    Intial

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,13 +1,16 @@
 function Navbar(){
     return(
-        <div className="w-screen h-16 bg-white p-0 m-0">
-            <div className="w-24 font-bold text-[#3572EF] left-0 m-1">
-                Vijayanta Alumni
+        <div class="flex justify-between h-16 px-3 m-auto max-w-6xl border-b-slate-500 border-b-2">
+            <div class="flex items-center">
+                <a href="#" class="flex-shrink-0 font-bold ">
+                    VIJAYANTA ALUMNI
+                </a>
             </div>
-            <div className="right-0 flex align-middle">
-                <button>
-                    click me
-                </button>
+            <div class="flex items-center space-x-4">
+                <a href="#" class="text-gray-700  bg-white p-4 hover:text-white hover:bg-[#3572EF] hover:font-bold">Home</a>
+                <a href="#" class="text-gray-700  bg-white p-4 hover:text-white hover:bg-[#3572EF] hover:font-bold">Events</a>
+                <a href="#" class="text-gray-700  bg-white p-4 hover:text-white hover:bg-[#3572EF] hover:font-bold">Gallery</a>
+                <a href="#" class="text-gray-700  bg-white p-4 hover:text-white hover:bg-[#3572EF] hover:font-bold">Sign Up</a>
             </div>
         </div>
     )
