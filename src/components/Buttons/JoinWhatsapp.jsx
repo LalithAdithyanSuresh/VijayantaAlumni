6754f74b98b0f1ea511c46008ff00f4c547893f2
Lalith Adithyan Suresh <140822743+LalithAdithyanSuresh@users.noreply.github.com>
2024-09-11 21:17:17 +0530

    Aded Pulse Button

--- src/components/Buttons/JoinWhatsapp.jsx
+++ src/components/Buttons/JoinWhatsapp.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
 import WA_Logo from "@assets/Logo/Whatsapp.svg";
-// COLOR : #43b740   ( Whatsapp Green )
 function JoinWhatsapp({link}) {
+    const WA_Color = "#43b740";
+    const Size = 28;
     return ( 
         <>
-        <div className='fixed bottom-2 right-2 w-4 h-4 p-2'>
-            <WA_Logo></WA_Logo>
+        <div className={`PulseSize fixed bottom-2 right-2  p-2 h-${Size} aspect-square animate-PulseSize `}>
+
+            <a href={link} target="_blank" rel="noopener noreferrer">
+                <img src={WA_Logo} alt="WhatsApp Logo" />
+            </a>
+            <div className={`bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))]   from-[${WA_Color}]  via-transparent to-transparent scale-150 absolute h-${Size} aspect-square top-0 left-0`}>
+            </div>
         </div>
         </>
      );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'PulseSize': 'zoomInOut 2s linear infinite',
+      },
+      keyframes: {
+        zoomInOut:{
+          '0% , 100%' : {scale : '1.1'},
+          '50%' : {scale:'1'},
+        }
+      }
+    },
   },
   plugins: [],
 }
