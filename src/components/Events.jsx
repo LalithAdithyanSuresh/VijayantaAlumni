c55461a7c0c61f38dc00c7a1b9cb6122029a7009
Sanjivan <sanjivan.nithiraj@gmail.com>
2024-09-11 23:29:44 +0530

    testing event page

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -18,6 +18,76 @@ const Events = () => {
             Nèi nèi gè nèi nèi <br></br>
             nèi gè nèi gè nèi nèi
         </EventCard>
+        <EventCard date={'12'} month={'Sep'} header={'Sunshine Rainbow White Pony'}>
+            Nǐ shì <br></br>
+            nèi nèi gè nèi nèi <br></br>
+            nèi gè nèi gè nèi nèi <br></br>
+            Nèi nèi gè nèi nèi <br></br>
+            nèi gè nèi gè nèi nèi
+        </EventCard>
+        <EventCard date={'12'} month={'Sep'} header={'Sunshine Rainbow White Pony'}>
+            Nǐ shì <br></br>
+            nèi nèi gè nèi nèi <br></br>
+            nèi gè nèi gè nèi nèi <br></br>
+            Nèi nèi gè nèi nèi <br></br>
+            nèi gè nèi gè nèi nèi
+        </EventCard>
+        <EventCard date={'12'} month={'Sep'} header={'Sunshine Rainbow White Pony'}>
+            Nǐ shì <br></br>
+            nèi nèi gè nèi nèi <br></br>
+            nèi gè nèi gè nèi nèi <br></br>
+            Nèi nèi gè nèi nèi <br></br>
+            nèi gè nèi gè nèi nèi
+        </EventCard>
+        <EventCard date={'12'} month={'Sep'} header={'Sunshine Rainbow White Pony'}>
+            Nǐ shì <br></br>
+            nèi nèi gè nèi nèi <br></br>
+            nèi gè nèi gè nèi nèi <br></br>
+            Nèi nèi gè nèi nèi <br></br>
+            nèi gè nèi gè nèi nèi
+        </EventCard>
+        <EventCard date={'12'} month={'Sep'} header={'Sunshine Rainbow White Pony'}>
+            Nǐ shì <br></br>
+            nèi nèi gè nèi nèi <br></br>
+            nèi gè nèi gè nèi nèi <br></br>
+            Nèi nèi gè nèi nèi <br></br>
+            nèi gè nèi gè nèi nèi
+        </EventCard>
+        <EventCard date={'12'} month={'Sep'} header={'Sunshine Rainbow White Pony'}>
+            Nǐ shì <br></br>
+            nèi nèi gè nèi nèi <br></br>
+            nèi gè nèi gè nèi nèi <br></br>
+            Nèi nèi gè nèi nèi <br></br>
+            nèi gè nèi gè nèi nèi
+        </EventCard>
+        <EventCard date={'12'} month={'Sep'} header={'Sunshine Rainbow White Pony'}>
+            Nǐ shì <br></br>
+            nèi nèi gè nèi nèi <br></br>
+            nèi gè nèi gè nèi nèi <br></br>
+            Nèi nèi gè nèi nèi <br></br>
+            nèi gè nèi gè nèi nèi
+        </EventCard>
+        <EventCard date={'12'} month={'Sep'} header={'Sunshine Rainbow White Pony'}>
+            Nǐ shì <br></br>
+            nèi nèi gè nèi nèi <br></br>
+            nèi gè nèi gè nèi nèi <br></br>
+            Nèi nèi gè nèi nèi <br></br>
+            nèi gè nèi gè nèi nèi
+        </EventCard>
+        <EventCard date={'12'} month={'Sep'} header={'Sunshine Rainbow White Pony'}>
+            Nǐ shì <br></br>
+            nèi nèi gè nèi nèi <br></br>
+            nèi gè nèi gè nèi nèi <br></br>
+            Nèi nèi gè nèi nèi <br></br>
+            nèi gè nèi gè nèi nèi
+        </EventCard>
+        <EventCard date={'12'} month={'Sep'} header={'Sunshine Rainbow White Pony'}>
+            Nǐ shì <br></br>
+            nèi nèi gè nèi nèi <br></br>
+            nèi gè nèi gè nèi nèi <br></br>
+            Nèi nèi gè nèi nèi <br></br>
+            nèi gè nèi gè nèi nèi
+        </EventCard>
     </div>
 
     
