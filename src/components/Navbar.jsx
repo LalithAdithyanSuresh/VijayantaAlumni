45c8e8b3df4cc9489fd24ab7da1f5e5e53676672
Sanjivan <sanjivan.nithiraj@gmail.com>
2024-09-11 17:43:32 +0530

    Navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 import React, {useState} from 'react';
 import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
-import NavLinks from './Buttons/NavLinks.jsx';
-import { NavLink } from 'react-router-dom';
+ import { NavLink } from 'react-router-dom';
 function Navbar(){
-    const [page,setPage] = useState(0);
+    const buttonClass = ({ isActive }) =>
+        isActive
+            ? 'bg-white  p-2 m-2  text-[#3572EF] font-medium cursor-pointer'
+            : 'text-gray-700  bg-white p-2 m-2 hover:text-[#3572EF] font-medium cursor-pointer transition-all duration 300 ease-linear';
+    const signButtonClass = ({ isActive }) =>
+        isActive
+            ? 'text-white  bg-[#3572EF] p-2 font-medium cursor-pointer border-2 border-[#3572EF] rounded m-2'
+            : 'text-white  bg-[#3572EF] p-2 m-2 hover:text-[#3572EF] hover:bg-white font-medium border-2 border-[#3572EF] rounded cursor-pointer transition-all duration 300 ease-linear';
     const [nav,setNav] = useState(true)
     const handleNav = () => {
         setNav(!nav)
@@ -13,30 +19,46 @@ function Navbar(){
         <div className="fixed top-0 left-0 w-screen flex justify-center" id="navbar">
             <div className="flex justify-between h-16 px-3 m-auto max-w-[1240px] items-center border-b-[#A7E6FF] border-b-2 backdrop-blur-sm w-full">
                 <div className="flex items-center">
-                    <a href="#navbar" className="flex-shrink-0 text-2xl font-bold text-[#3572EF] ">
+                    <NavLink to={'/'} className="flex-shrink-0 text-2xl font-bold text-[#3572EF] ">
                         VIJAYANTA ALUMNI
-                    </a>
+                    </NavLink>
                 </div>
                 <div className="hidden md:flex items-center space-x-4">
-                    <NavLinks text={"Home"} code={0} c={page==0 ? 1 : 0} setPage={setPage} route={'/'}></NavLinks>
-                    <NavLinks text={"Events"} code={1} c={page==1 ? 1 : 0} setPage={setPage} route={'events'}></NavLinks>
-                    <NavLinks text={"Gallery"} code={2} c={page==2 ? 1 : 0} setPage={setPage} route={'gallery'}></NavLinks>
-                    <NavLink to={'/signup'} className={page==3 ? "text-white  bg-[#3572EF] p-2 font-medium cursor-pointer border-2 border-[#3572EF] rounded m-2" : "text-white  bg-[#3572EF] p-2 m-2 hover:text-[#3572EF] hover:bg-white font-medium border-2 border-[#3572EF] rounded  cursor-pointer"}>Sign Up</NavLink>
+                    <NavLink to='/' className={buttonClass}>
+                        Home
+                    </NavLink>
+                    <NavLink to='/events' className={buttonClass}>
+                        Events
+                    </NavLink>
+                    <NavLink to='/gallery' className={buttonClass}>
+                        Gallery
+                    </NavLink>
+                    <NavLink to='/signup' className={signButtonClass}>
+                        Signup
+                    </NavLink>
                 </div>
                 <div onClick={handleNav} className='block md:hidden'>
                     {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20}/>}
                 </div>
-            </div>
-                <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r-2 border-r-gray-200 flex flex-col uppercase p-4 bg-white ease-in-out duration-500' : 'fixed left-[-100%] top-0 flex flex-col ease-in-out duration-500'}>
-                    <a href="#navbar" className="flex-shrink-0 text-2xl font-bold text-[#3572EF] mb-8 ">
-                        VIJAYANTA ALUMNI
-                    </a>
-                    <NavLinks text={"Home"} code={0} c={page==0 ? 1 : 0} setPage={setPage} handleNav={handleNav} route={'/'}></NavLinks>
-                    <NavLinks text={"Events"} code={1} c={page==1 ? 1 : 0} setPage={setPage} handleNav={handleNav} route={'/events'}></NavLinks>
-                    <NavLinks text={"Gallery"} code={2} c={page==2 ? 1 : 0} setPage={setPage} handleNav={handleNav} route={'/gallery'}></NavLinks>
-                    <NavLinks text={"Sign Up"} code={3} c={page==3 ? 1 : 0} setPage={setPage} handleNav={handleNav} route={'/signup'}></NavLinks>
                 </div>
-        </div>
+                <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r-2 border-r-gray-200 flex flex-col uppercase p-4 bg-white ease-in-out duration-500' : 'fixed left-[-100%] top-0 flex flex-col ease-in-out duration-500'}>
+                    <NavLink to={'/'} className="flex-shrink-0 text-2xl font-bold text-[#3572EF] mb-8 ">
+                        VIJAYANTA ALUMNI
+                    </NavLink>
+                    <NavLink to='/' className={buttonClass} onClick={handleNav}>
+                        Home
+                    </NavLink>
+                    <NavLink to='/events' className={buttonClass} onClick={handleNav}>
+                        Events
+                    </NavLink>
+                    <NavLink to='/gallery' className={buttonClass} onClick={handleNav}>
+                        Gallery
+                    </NavLink>
+                    <NavLink to='/signup' className={signButtonClass} onClick={handleNav}>
+                        Signup
+                    </NavLink>
+                </div>
+            </div>
         </>
     );
 }
